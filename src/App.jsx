7f454778be9cef39f199b28b7fe7d8e9aa3516c8
abdelghani bahri <bahri.ghanou@gmail.com>
2025-05-30 14:43:00 +0100

    add raleway font

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 // import { useState } from 'react'
-import './App.css'
+// import './App.css'
 import { useAppcontext } from './store/state'
 
 function App() {
   const {count, setCount} = useAppcontext()
   return (
     <>
-      <div className='bg-white dark:bg-black'>
+      <div className='bg-white dark:bg-black font-raleway'>
         
       </div>
-      <h1 className='font-bold text-5xl '>Vite + React</h1>
-      <div className="card">
+      <h1 className='font-bold text-5xl font-raleway '>Vite + React</h1>
+      <div className="card  font-raleway">
         <button onClick={() => setCount((count) => count + 5)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Women <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
